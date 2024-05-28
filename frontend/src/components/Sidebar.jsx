6178975f769280e33a77c81dcc7ccfd9f71caaea
Vishal-satpute2122 <vishal.s@infogen-labs.com>
2024-05-28 09:42:20 +0530

    commenting upload files

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -107,12 +107,12 @@ const Sidebar = ({ sidebarToggle, role }) => {
           </li>
         )}
 
-        <li className=" rounded hover:shadow hover:bg-blue-500 py-2 leftBtn">
+        {/* <li className=" rounded hover:shadow hover:bg-blue-500 py-2 leftBtn">
           <Link to="/uploadFile" className="px-3">
             <FaList className="inline-block w-6 h-6 mr-2 -mt-2"></FaList>
             Upload Files
           </Link>
-        </li>
+        </li> */}
 
         <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 leftBtn">
           <Link to="/retrospective" className="px-3">
