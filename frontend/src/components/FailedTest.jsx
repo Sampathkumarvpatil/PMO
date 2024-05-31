aa94f3739adc2405e60c241e5ffa72f3747b091e
Danish kohli <144088574+danishkohli123@users.noreply.github.com>
2024-05-31 23:08:47 +0530

    exception pop up done ,graph done in failed pages

--- frontend/src/components/FailedTest.jsx
+++ frontend/src/components/FailedTest.jsx
@@ -1,36 +1,97 @@
 import TestIcons from "./TestIcons";
+import React, { useState } from "react";
+import { FaArrowCircleDown } from "react-icons/fa";
+import { useFetchTestReport } from "../utils/useFetchTestReport";
+import ExecutionTimeGraph from "../Graphs/ExecutionTimeGraph";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 function FailedTest({ sidebarToggle }) {
+    const { data } = useFetchTestReport();
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+    // const { data } = useFetchTestReport();
     return (
         <div
             className={`transition-all duration-300 ${sidebarToggle ? "ml-0" : "ml-64"
                 }`}
         >
             <div className="grid grid-cols-[5%,94%] justify-between">
-                <div style={{backgroundColor:'#e2e3f3' }}>
+                <div style={{ backgroundColor: '#e2e3f3' }}>
                     <TestIcons />
                 </div>
-                <div className="grid grid-cols-[12%,38%,50%] mt-4 px-4">
+                <div className="grid grid-cols-[38%,47%,15%] mt-4 px-4">
                     <div style={{ borderRight: '1px solid' }}>
                         <h3 className="rounded-lg inline-block px-2 py-1" style={{ backgroundColor: '#aee7ae', color: 'green' }}>TIMESTAMP</h3>
-                        <h1 className="px-4 py-8" >7:55:28 AM</h1>
+                        <h1 className="px-4 py-4" >7:55:28 AM</h1>
+                        <div style={{ padding: "0px 18px" }}>
+                            <ExecutionTimeGraph data={data} />
+                        </div>
                     </div>
                     <div style={{ borderRight: '1px solid' }}>
                         <h3 className="ml-2 rounded-lg inline-block px-2 py-1" style={{ backgroundColor: '#aee7ae', color: 'green' }}>SCREENSHOT</h3>
-                        <img src="../dummy_img.png" width={500} style={{ height: '300px' }} className="ml-2 px-4 py-8"></img>
+                        <img src="../dummy_img.png" width={700} style={{ height: '350px' }} className="ml-2 px-4 py-8"></img>
                     </div>
                     <div>
-                        <h3 className="ml-2 rounded-lg inline-block px-2 py-1" style={{ backgroundColor: '#aee7ae', color: 'green' }}>EXCEPTION</h3>
-                        <p className="ml-2 px-4 py-8 text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati, culpa officiis est aliquid molestiae repudiandae. Nobis ipsam eos, sunt cum praesentium eius tenetur numquam a laborum, consequatur eligendi porro?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis animi et vitae nostrum nobis rem vel, cum amet aliquid rerum sint maxime error dolor itaque iure. Voluptas minima quo laboriosam!</p>
-                    </div>
-                    <div style={{ borderRight: '1px solid' }}>
-                        <h1 className="px-4 py-8" >7:55:28 AM</h1>
-                    </div>
-                    <div style={{ borderRight: '1px solid' }}>
-                        <img src="../dummy_img.png" width={500} style={{ height: '300px' }} className="ml-2 px-4 py-8"></img>
-                    </div>
-                    <div>
-                        <p className="ml-2 px-4 py-8 text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati, culpa officiis est aliquid molestiae repudiandae. Nobis ipsam eos, sunt cum praesentium eius tenetur numquam a laborum, consequatur eligendi porro?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis animi et vitae nostrum nobis rem vel, cum amet aliquid rerum sint maxime error dolor itaque iure. Voluptas minima quo laboriosam!</p>
+                        <h3 className="ml-2 rounded-lg inline-block px-2 py-1" style={{ backgroundColor: '#aee7ae', color: 'green' }}>DETAILS</h3>
+                        <div className="py-4">
+                            <div className="flex flex-row ml-4 mt-3">
+                                <h3>ProjectName :</h3>
+                                <p>&nbsp;PMO</p>
+                            </div>
+                            <div className="flex flex-row ml-4 mt-3">
+                                <h3>ClassName&nbsp;&nbsp;&nbsp;&nbsp; :</h3>
+                                <p>&nbsp;Data</p>
+                            </div>
+                            <div className="flex flex-row ml-4 mt-3">
+                                <h3>MethodName :</h3>
+                                <p>&nbsp;Hello</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row justify-center items-center ml-4 my-2 rounded-lg" style={{ backgroundColor: '#edcbed', width: '50%' }}>
+                            <h3 className="px-1 py-2 text-lg cursor-pointer" style={{ color: 'purple' }} onClick={handleOpenDialog} >Exception</h3>
+                            <Dialog
+                                open={openDialog}
+                                onClose={handleCloseDialog}
+                                fullWidth
+                            >
+                                <div className="flex flex-row justify-between">
+                                    <DialogTitle
+                                        className="!pb-0 !pt-3"
+                                        style={{
+                                            fontWeight: "bold",
+                                            fontSize: "1.5rem",
+                                            textAlign: "center",
+                                        }}
+                                    >
+                                        Exception
+                                    </DialogTitle>
+                                    <button className=" bg-red-600 rounded-full text-white mt-2 mr-8" onClick={handleCloseDialog}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                        {/* <FaPlusCircle className=" flex justify-end text-xl" onClick={onClose}/> */}
+
+                                    </button>
+                                </div>
+                                <DialogContent>
+                                    <textArea rows={10} cols={60} >
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati, culpa officiis est aliquid molestiae repudiandae. Nobis ipsam eos, sunt cum praesentium eius tenetur numquam a laborum, consequatur eligendi porro?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis animi et vitae nostrum nobis rem vel, cum amet aliquid rerum sint maxime error dolor itaque iure. Voluptas minima quo laboriosam!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde obcaecati, culpa officiis est aliquid molestiae repudiandae. Nobis ipsam eos, sunt cum praesentium eius tenetur numquam a laborum, consequatur eligendi porro?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis animi et vitae nostrum nobis rem vel, cum amet aliquid rerum sint maxime error dolor itaque iure. Voluptas minima quo laboriosam!
+                                    </textArea>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/TestResult.jsx
+++ frontend/src/components/TestResult.jsx
@@ -19,32 +19,32 @@ function TestResult({ sidebarToggle }) {
     <div className="">
       <div className="flex justify-between">
         <div
-          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg"
-          style={{ backgroundColor: "#b6e7e9" }}
+          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg transform hover:scale-105 transition-transform duration-300"
+          style={{ backgroundColor: "rgb(208 234 235)" }}
         >
           <div className="flex flex-row">
             <h2 className="text-base mb-2">Project Name:</h2>
-            <p className="text-sm" style={{marginTop:'3px'}}>
+            <p className="text-sm" style={{ marginTop: '3px' }}>
               {data?.body?.project_name}
             </p>
           </div>
           <div className="flex flex-row">
             <h2 className="text-base mb-2">Class Name:</h2>
-            <p className="text-sm" style={{marginTop:'2px'}}>
+            <p className="text-sm" style={{ marginTop: '2px' }}>
               {data?.body?.class_name}
             </p>
           </div>
         </div>
         <div
-          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg"
-          style={{ backgroundColor: "#b6e7e9" }}
+          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg transform hover:scale-105 transition-transform duration-300"
+          style={{ backgroundColor: "rgb(216 216 242)" }}
         >
           <h2 className="text-base mb-2">Total Test Cases</h2>
           <p className="text-base">{data?.body?.total_test_cases}</p>
         </div>
         <div
-          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg"
-          style={{ backgroundColor: "#c0ebc0" }}
+          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg transform hover:scale-105 transition-transform duration-300"
+          style={{ backgroundColor: "rgb(232 255 232)" }}
         >
           <h2 className="text-base mb-2">Test passed</h2>
           <p className="text-base" style={{ color: "green" }}>
@@ -52,8 +52,8 @@ function TestResult({ sidebarToggle }) {
           </p>
         </div>
         <div
-          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg"
-          style={{ backgroundColor: "#efc7c7" }}
+          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg transform hover:scale-105 transition-transform duration-300"
+          style={{ backgroundColor: "rgb(243 230 230)" }}
         >
           <h2 className="text-base mb-2">Test Failed</h2>
           <p className="text-base" style={{ color: "red" }}>
@@ -61,8 +61,8 @@ function TestResult({ sidebarToggle }) {
           </p>
         </div>
         <div
-          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg"
-          style={{ backgroundColor: "#e9f5a9" }}
+          className="mt-8 border shadow px-4 py-3 flex-1 mr-4 rounded-lg transform hover:scale-105 transition-transform duration-300"
+          style={{ backgroundColor: "rgb(251 255 228)" }}
         >
           <h2 className="text-base mb-2">Test Skipped </h2>
           <p className="text-base" style={{ color: "red" }}>
@@ -72,19 +72,19 @@ function TestResult({ sidebarToggle }) {
       </div>
       <div className="grid grid-cols-[49%,49%] justify-between my-6 ">
         <div style={{ paddingTop: "44px", border: "1px solid" }}>
-          <CylinderGraph data={data} />
+          <WordCloud data={data} />
         </div>
         <div
           className="chart-container"
           style={{ paddingTop: "44px", border: "1px solid" }}
         >
-          <ItemChart data={data} />
+          <CylinderGraph data={data} />
         </div>
         <div
           className="chart-container"
           style={{ paddingTop: "44px", border: "1px solid", marginTop: "30px" }}
         >
-          <WordCloud data={data} />
+          <ItemChart data={data} />
         </div>
         <div
           className="chart-container"
