7ba6654a2e8515957df3bce30bb91ca760aa11f7
Vishal-satpute2122 <vishal.s@infogen-labs.com>
2024-06-05 11:42:18 +0530

    add new image

--- frontend/src/loginForm/Header.jsx
+++ frontend/src/loginForm/Header.jsx
@@ -1,28 +1,28 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Header({
-    heading,
-    paragraph,
-    linkName,
-    linkUrl="#"
-}){
-    return(
-        <div className="mb-10">
-            <div className="flex justify-center">
-                <img 
-                    alt=""
-                    className="h-14 w-40"
-                    src="https://corp.infogen-labs.com/images/logo_infogen_gray.png"/>
-            </div>
-            <h2 className="mt-6 text-center text-3xl font-light text-white">
-                {heading}
-            </h2>
-            <p className=" text-center text-sm text-gray-100 mt-5">
-            {paragraph} {' '}
-            <Link to={linkUrl} className="font-medium text-red-600 hover:text-black">
-                {linkName}
-            </Link>
-            </p>
-        </div>
-    )
+  heading,
+  paragraph,
+  linkName,
+  linkUrl = "#",
+}) {
+  return (
+    <div className="mb-10">
+      <div className="flex justify-center">
+        <img alt="" className="h-14 w-20" src="./image3.png" />
+      </div>
+      <h2 className="mt-6 text-center text-3xl font-light text-white">
+        {heading}
+      </h2>
+      <p className=" text-center text-sm text-gray-100 mt-5">
+        {paragraph}{" "}
+        <Link
+          to={linkUrl}
+          className="font-medium text-red-600 hover:text-black"
+        >
+          {linkName}
+        </Link>
+      </p>
+    </div>
+  );
 }
