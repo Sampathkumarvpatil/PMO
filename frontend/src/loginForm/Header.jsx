fb375fe056597cf2a44db471c61797ff972d54c6
Vishal-satpute2122 <vishal.s@infogen-labs.com>
2024-06-24 12:17:57 +0530

    Update logo

--- frontend/src/loginForm/Header.jsx
+++ frontend/src/loginForm/Header.jsx
@@ -9,7 +9,7 @@ export default function Header({
   return (
     <div className="mb-10">
       <div className="flex justify-center">
-        <img alt="" className="h-14 w-20" src="./image3.png" />
+        <img alt="" className="h-14 w-20" src="./fevicon.png" />
       </div>
       <h2 className="mt-6 text-center text-3xl font-light text-white">
         {heading}
