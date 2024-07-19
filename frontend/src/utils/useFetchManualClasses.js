ad37d4fec5ca950a781c798f0fc0816aad5b82e4
Vishal-satpute2122 <vishal.s@infogen-labs.com>
2024-07-19 13:34:46 +0530

    Create hook to fetch manual classes

---
+++ frontend/src/utils/useFetchManualClasses.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export const useFetchManualClasses = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function fetchClasses(project_name) {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        process.env.REACT_APP_FETCH_MANUAL_CLASSES ?? "",
+        {
+          method: "POST",
+
+          body: JSON.stringify({ project_name }),
+        }
+      );
+
+      if (!res.ok)
+        throw new Error(
+          "Project does not exist or classes not found for the given project!!"
+        );
+      const jsonResponse = await res.json();
+
+      setData(jsonResponse);
+      setError(null);
+      return jsonResponse;
+    } catch (e) {
+      console.error(
+        e?.message || "Error while fetching classes data from s3.."
+      );
+      setError(e?.message || "Error while fetching classes data from s3..");
+    } finally {
+      setLoading(false);
+    }
+  }
+  return { data, error, fetchClasses, loading };
+};
